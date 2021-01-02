1c393b3385b4509a3d8328f1e4954a0bb2f3ebf9
t3xhno <t3xhnolainx@gmail.com>
2021-01-02 03:28:08 +0100

    Day3 solution1

--- day3/src.cpp
+++ day3/src.cpp
@@ -1,19 +1,18 @@
 #include <fstream>
 #include <iostream>
-#include <vector>
 #include <string>
 using namespace std;
 
 void solve() {
     int solution_1{ 0 }, solution_2{ 0 };
-    vector<string> myMap{  };
     ifstream file;
     file.open("./day3/input.dat");
     if (file.is_open()) {
+        int i{ 0 }, j{ 0 };
         for (string line; getline(file, line);) {
-
-            // THE CODES GO HERE
-
+            if (i++ == 0) continue;
+            j += 3;
+            if (line[j % line.length()] == '#') solution_1++;
         }
     }
     if (file.is_open()) file.close();
