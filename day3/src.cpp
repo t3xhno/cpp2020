b96ad7a5aed41a82f60f91c1489b04bccecd053c
t3xhno <t3xhnolainx@gmail.com>
2021-01-02 04:22:11 +0100

    Day3 done

--- day3/src.cpp
+++ day3/src.cpp
@@ -2,18 +2,15 @@
 #include <iostream>
 using namespace std;
 
-#define ll long long
-
 void solve() {
-    ll solution_1{ 0 }, solution_2{ 1 };
+    long long solution_1{ 0 }, solution_2{ 1 };
     int steps[5][2] = {{1, 1}, {1, 3}, {1, 5}, {1, 7}, {2, 1}};
     ifstream file;
     file.open("./day3/input.dat");
     if (file.is_open()) {
         for (int k = 0; k < 5; k++) {
             file.clear(); file.seekg(0);
-            int i{ 0 }, j{ 0 };
-            int total{ 0 };
+            int i{ 0 }, j{ 0 }, total{ 0 };
             int i_step{ steps[k][0] }, j_step{ steps[k][1] };
             for (string line; getline(file, line);) {
                 if (i++ % i_step != 0) {
@@ -35,8 +32,6 @@ void solve() {
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
     solve();
-
     return 0;
 }
